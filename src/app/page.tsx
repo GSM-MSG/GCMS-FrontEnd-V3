733358d572d931d3a465d7efcf6fef04d8b0c6c6
Ethen1264 <jxorhks86909763@gmail.com>
2024-09-12 15:10:13 +0900

    🗂️ Chore: set up initial project folder structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
-import { Button } from '@/components/ui/button';
-import TestButton from '@/stories/atoms/TestButton';
+import { Checkbox } from '@/components/ui/checkbox';
+import TestButton from '@/shared/atoms/TestButton';
+import Text from '@/shared/atoms/Text';
 
 export default function Home() {
   return (
     <div>
-      <Button>하이</Button>
+      <div className="flex items-center space-x-2">
+        <Checkbox id="terms" />
+        <label
+          htmlFor="terms"
+          className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+        >
+          Accept terms and conditions
+        </label>
+      </div>
       <TestButton />
+      <Text />
     </div>
   );
 }
