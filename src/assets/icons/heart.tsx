df38538936342a1e86a80d4934e1d5bce1ea3482
Ethen1264 <jxorhks86909763@gmail.com>
2024-09-16 16:42:22 +0900

    🎨Update: Change component styles

---
+++ src/assets/icons/heart.tsx
@@ -0,0 +1,33 @@
+const heart = () => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_261_8865)">
+        <path
+          d="M12.005 18.1516L5.74854 12.4846C2.3483 9.08435 7.34664 2.5559 12.005 7.8376C16.6632 2.5559 21.639 9.10702 18.2614 12.4846L12.005 18.1516Z"
+          stroke="#9F9FA8"
+          strokeWidth="1.4"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_261_8865">
+          <rect
+            width="16"
+            height="16"
+            fill="white"
+            transform="translate(4 4)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default heart;
