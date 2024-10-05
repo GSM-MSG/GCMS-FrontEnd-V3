74e749c22d647ea6f58721b84edb499a468cf7a2
happytaeyoon <s23021@gsm.hs.kr>
2024-10-06 01:29:43 +0900

    add UserProfileCard

---
+++ src/components/Common/UserProfileCard/index.stories.ts
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from '@storybook/react';
+import UserProfileCard from './index';
+
+const config: Meta<typeof UserProfileCard> = {
+  title: 'UserProfileCard',
+  component: UserProfileCard,
+};
+
+export default config;
+
+type Story = StoryObj<typeof UserProfileCard>;
+
+export const Primary: Story = {
+  args: {
+    profileUrl:
+      'https://img.vogue.co.kr/vogue/2019/09/style_5d6f22585f074-930x619.png',
+    role: '동아리 부장',
+    name: '장예슬',
+  },
+};
