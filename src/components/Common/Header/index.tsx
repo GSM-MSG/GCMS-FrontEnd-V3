91440f929c0e090e881a9af0de35d59767e784be
민우석 <s23044@gsm.hs.kr>
2024-10-08 13:50:43 +0900

    ✨ Add responsive

--- src/components/Common/Header/index.tsx
+++ src/components/Common/Header/index.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
 import React from 'react';
+import { Club, Community, Home, Notice, PersonRound } from '@/assets/icons';
 import TestProfileImg from '@/assets/png/testProfileImg.png';
 
 interface Props {
@@ -14,15 +15,15 @@ const Header = ({ role }: Props) => {
   const route = useRouter();
 
   return (
-    <header className="flex items-center justify-center bg-black py-4">
-      <div className="flex w-[700px] justify-between">
+    <header className="flex items-center justify-center bg-black py-4 mobile:px-5 mobile:py-3">
+      <div className="flex w-full max-w-[700px] justify-between">
         <p
           onClick={() => route.push('/')}
-          className="cursor-pointer text-center text-h3 text-main-500"
+          className="cursor-pointer text-center text-h3 text-main-500 mobile:text-h4"
         >
           GCMS
         </p>
-        <div className="flex items-center gap-12">
+        <div className="flex items-center gap-12 mobile:hidden">
           {role === 'student' && (
             <>
               <p
@@ -59,6 +60,23 @@ const Header = ({ role }: Props) => {
             onClick={() => route.push('my')}
             className="cursor-pointer rounded-full text-body2 text-gray-600"
           />
+        </div>
+        <div className="hidden mobile:fixed mobile:bottom-0 mobile:left-0 mobile:flex mobile:w-full mobile:justify-between mobile:bg-black mobile:px-5">
+          <div onClick={() => route.push('/')} className="px-5 py-4">
+            <Home current={path === '/'} />
+          </div>
+          <div onClick={() => route.push('/myClub')} className="px-5 py-4">
+            <Club current={path === '/myClub'} />
+          </div>
+          <div onClick={() => route.push('/community')} className="px-5 py-4">
+            <Community current={path === '/community'} />
+          </div>
+          <div onClick={() => route.push('/notice')} className="px-5 py-4">
+            <Notice current={path === '/notice'} />
+          </div>
+          <div onClick={() => route.push('/my')} className="px-5 py-4">
+            <PersonRound current={path === '/my'} />
+          </div>
         </div>
       </div>
     </header>
