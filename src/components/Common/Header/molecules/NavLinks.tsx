9b852249b3543ea1509685848e75ab87ba646bb2
민우석 <s23044@gsm.hs.kr>
2024-10-09 13:12:10 +0900

    ♻️ Refactor: navitems mapping

--- src/components/Common/Header/molecules/NavLinks.tsx
+++ src/components/Common/Header/molecules/NavLinks.tsx
@@ -11,27 +11,24 @@ const NavLinks = ({ role }: Props) => {
   const route = useRouter();
   const path = usePathname();
 
+  const navItems = [
+    { path: '/myClub', text: '내 동아리', role: 'student' },
+    { path: '/community', text: '커뮤니티', role: 'student' },
+    { path: '/notice', text: '공지사항', role: 'all' },
+  ];
+
   return (
     <div className="flex items-center gap-12 mobile:hidden">
-      {role === 'student' && (
-        <>
+      {navItems
+        .filter((item) => item.role === role || item.role === 'all')
+        .map((item) => (
           <NavLink
-            current={path === '/myClub'}
-            onClick={() => route.push('/myClub')}
-            text="내 동아리"
+            key={item.text}
+            current={path === item.path}
+            onClick={() => route.push(item.path)}
+            text={item.text}
           />
-          <NavLink
-            current={path === '/community'}
-            onClick={() => route.push('/community')}
-            text="커뮤니티"
-          />
-        </>
-      )}
-      <NavLink
-        current={path === '/notice'}
-        onClick={() => route.push('/notice')}
-        text="공지사항"
-      />
+        ))}
       <ProfileImage src={TestProfileImg} />
     </div>
   );
