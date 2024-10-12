753759dae32fddf8fe1323459015c405a2c5c891
김태윤 <kimtaeyoon@gimtaeyun-ui-MacBookPro.local>
2024-10-13 00:58:27 +0900

    🎨 Change img->Image

--- src/components/Common/UserProfileCard/index.tsx
+++ src/components/Common/UserProfileCard/index.tsx
@@ -1,20 +1,20 @@
-interface UserProfileCardProps {
+import Image from 'next/image';
+
+interface Props {
   profileUrl: string;
   role: string;
   name: string;
 }
 
-const UserProfileCard: React.FC<UserProfileCardProps> = ({
-  profileUrl,
-  role,
-  name,
-}) => {
+const UserProfileCard = ({ profileUrl, role, name }: Props) => {
   return (
     <div className="flex h-[62px] w-[119px] rounded-md bg-[#232327] p-3">
-      <img
+      <Image
         src={profileUrl}
         alt="Profile"
         className="mr-1 h-9 w-9 rounded-full object-cover"
+        width={36}
+        height={36}
       />
       <div className="w-[55px]">
         <p className="font-pretendard text-left text-[10px] font-normal leading-[18px] text-[#FFFFFF66]">
